8980f1dbe754c143c98ae272117a72e94622005d
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-14 19:57:03 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {name:"Will",streetAddress:"Baringo"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee, [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee};
+    newEmployee[key];
+    delete newEmployee[key];
+    
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    employee[key];
+    delete employee[key];
+
+    return employee;
+};
